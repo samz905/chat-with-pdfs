5c20cb5bfac44a89e1b9fa08fa09d94ce1376bb8
Samarth Zalte <52416785+samz905@users.noreply.github.com>
2023-06-14 19:14:37 +0530

    chat logic and final touches

--- app.py
+++ app.py
@@ -4,6 +4,10 @@ from PyPDF2 import PdfReader
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.embeddings import OpenAIEmbeddings
 from langchain.vectorstores import FAISS
+from langchain.llms import OpenAI
+from langchain.chains.question_answering import load_qa_chain
+from langchain.callbacks import get_openai_callback
+
 
 def main():
     load_dotenv()
@@ -30,10 +34,23 @@ def main():
         chunks = text_splitter.split_text(text)
 
         # Create the embeddings
-        embeddings = OpenAIEmbeddings(chunks)
+        embeddings = OpenAIEmbeddings()
 
         # Build the vector store
         knowledge_base = FAISS.from_texts(chunks, embeddings)
+
+        # Chat functionality
+        user_input = st.text_input("Ask a question about your PDF:")
+        if user_input:
+            docs = knowledge_base.similarity_search(user_input)
+
+            chain = load_qa_chain(OpenAI(), chain_type="stuff")
+
+            with get_openai_callback() as cb:
+                response = chain.run(input_documents=docs, question=user_input)
+                print(cb)
+
+            st.write(response)
 
 
 if __name__ == '__main__':
